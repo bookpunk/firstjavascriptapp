a90aca8a55edaf24f5bf014d33b3532a297a2f7c
bookpunk <bookpunk@gmail.com>
2017-01-29 15:42:25 -0600

    clean up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // var fname = prompt("What's your first name?");
 fname = 'matt';
 var app = document.getElementById('fname');
-//document.getElementById('app').innerHTML = fname;
 app.innerHTML = fname;
 console.log('The first name entered is ' + fname)
