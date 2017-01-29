b7a60af8cfb3a47d6d5d130a6f3efaa5cd2dce69
bookpunk <bookpunk@gmail.com>
2017-01-29 15:35:19 -0600

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// var fname = prompt("What's your first name?");
+fname = 'matt';
+var app = document.getElementById('fname');
+//document.getElementById('app').innerHTML = fname;
+app.innerHTML = fname;
+console.log('The first name entered is ' + fname)
